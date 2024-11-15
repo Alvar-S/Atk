d3b59dc7093a5d3ac264c2a34213da8c0ebbe852
Alvar <alvar.stenroth@gmail.com>
2024-11-15 08:32:14 +0200

    astro config asestuksien päivitäminen.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ site: 'https://alvar-s.github.io',
-    base: 'Atk',});
+export default defineConfig({
+  // GitHub
+  // site: "https://alvar-s.github.io",
+  // base: "Atk",
+
+  // Palvelin
+  site: "https://nettisivukoti.fi",
+});
